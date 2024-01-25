0f944d6162274bd7bbe2f350a27632ddc4f0dfda
GhazzaiSkander <152886354+GhazzaiSkander@users.noreply.github.com>
2024-01-25 14:13:46 +0100

    Update __init__.py

--- dtlm/__init__.py
+++ dtlm/__init__.py
@@ -8,4 +8,5 @@ from .transformation import simple_transformation
 from .prompt import simple_pormpt_template
 from .completion import get_completion
 from .verification import Edges_Verification
+from .counter import split_list_by_token_limit
 
