bdbc81599680bdab7cc6dc5224c758dafaffca39
GhazzaiSkander <152886354+GhazzaiSkander@users.noreply.github.com>
2024-01-31 11:21:06 +0100

    Update __init__.py

--- dtlm/__init__.py
+++ dtlm/__init__.py
@@ -5,6 +5,7 @@
 
 from .history import log_experiment
 from .transformation import simple_transformation
+from .transformation import basic_transformation
 from .prompt import simple_pormpt_template
 from .completion import get_completion
 from .verification import Edges_Verification
